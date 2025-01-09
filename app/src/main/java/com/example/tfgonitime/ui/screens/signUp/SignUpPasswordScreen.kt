e09c8a4faaa4ff386925ed1b292e6f104e447a9a
Yilin <yilin.zh135@gmail.com>
2025-01-09 13:00:50 +0100

    Add all screens for SignUp

--- app/src/main/java/com/example/tfgonitime/ui/screens/signUp/SignUpPasswordScreen.kt
+++ app/src/main/java/com/example/tfgonitime/ui/screens/signUp/SignUpPasswordScreen.kt
@@ -5,6 +5,6 @@ import androidx.navigation.NavHostController
 import com.example.tfgonitime.viewmodel.AuthViewModel
 
 @Composable
-fun SignUpNameScreen(navHostController: NavHostController, authViewModel: AuthViewModel) {
+fun SignUpPasswordScreen(navHostController: NavHostController, authViewModel: AuthViewModel) {
 
 }
