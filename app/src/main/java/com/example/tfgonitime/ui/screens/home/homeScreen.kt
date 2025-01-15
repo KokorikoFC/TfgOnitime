685a8e6fdd53391f0b9dfb53c6026074f9af1965
kiwan33 <rumansillar@gmail.com>
2025-01-15 13:45:39 +0100

    Add scaffold to home and settings screens

--- app/src/main/java/com/example/tfgonitime/ui/screens/home/homeScreen.kt
+++ app/src/main/java/com/example/tfgonitime/ui/screens/home/homeScreen.kt
@@ -48,97 +48,22 @@ import com.example.tfgonitime.ui.components.CustomRadioButton
 
 
 @Composable
-fun HomeScreen(navHostController: NavHostController, authViewModel: AuthViewModel, languageViewModel: LanguageViewModel) {
-
-    val context = LocalContext.current
-
-    // Cargar el idioma al iniciar la pantalla
-    LaunchedEffect(Unit) {
-        languageViewModel.loadLocale(context)
-    }
-
-    val locale by languageViewModel.locale
-
-    val languages = listOf(
-        "Español (España)" to Locale("es"),
-        "Inglés (Reino Unido)" to Locale("en"),
-        "Gallego" to Locale("gl")
-    )
-
-    // Encuentra el idioma actual
-    var selectedLanguage by remember {
-        mutableStateOf(
-            languages.find { it.second.language == locale.language }?.first
-                ?: languages[0].first
-        )
-    }
-
-    // Actualizar el idioma seleccionado cuando se carga el idioma desde LanguageViewModel
-    LaunchedEffect(locale) {
-        selectedLanguage = languages.find { it.second.language == locale.language }?.first
-            ?: languages[0].first
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Idioma",
-            style = TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            ),
-            modifier = Modifier.padding(bottom = 24.dp)
-        )
-
-        Text(text = stringResource(R.string.forgot_password))
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        languages.forEach { (languageName, localeOption) ->
-            LanguageOption(
-                text = languageName,
-                isSelected = languageName == selectedLanguage,
-                onClick = {
-                    selectedLanguage = languageName
-                    LanguageManager.setLocale(context, localeOption)
-                    languageViewModel.setLocale(localeOption)
-                }
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-            authViewModel.logout {
-                navHostController.navigate("loginScreen") // Navega a la pantalla de login
+fun HomeScreen(navHostController: NavHostController) {
+    Scaffold(
+        containerColor = Color.White,
+        bottomBar = { CustomBottomNavBar(navHostController) },
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                // Aquí puedes agregar el contenido de la pantalla de inicio
             }
-        }) {
-            Text(text = "Cerrar sesión")
         }
-
-    }
+    )
 }
 
-@Composable
-fun LanguageOption(text: String, isSelected: Boolean, onClick: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = text,
-            style = TextStyle(fontSize = 16.sp, color = Color.Black),
-            modifier = Modifier.weight(1f)
-        )
-        CustomRadioButton(isSelected = isSelected, onClick = onClick)
-    }
-}
 
 
 
