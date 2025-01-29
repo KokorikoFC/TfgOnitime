6672fbf76ff00e38e264d84fc156b7337544a79a
Yilin <yilin.zh135@gmail.com>
2025-01-29 22:25:01 +0100

    Add logic for assigning taskGroup colors in Home and TaskItem

--- app/src/main/java/com/example/tfgonitime/ui/components/taskComp/TaskItem.kt
+++ app/src/main/java/com/example/tfgonitime/ui/components/taskComp/TaskItem.kt
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -26,24 +24,25 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.example.tfgonitime.data.model.Task
-import com.example.tfgonitime.ui.theme.DarkBrown
-import com.example.tfgonitime.ui.theme.Green
+import com.example.tfgonitime.ui.components.CustomCheckBox
+import com.example.tfgonitime.ui.theme.*
 import com.example.tfgonitime.viewmodel.TaskViewModel
 
 @Composable
 fun TaskItem(
     task: Task,
-    navHostController: NavHostController,
     userId: String,
     onDelete: () -> Unit,
     onEdit: () -> Unit,
     taskViewModel: TaskViewModel,
     index: Int,
-    totalItems: Int
+    totalItems: Int,
+    color: Color
 ) {
     var showPopup by remember { mutableStateOf(false) }
     var checked by remember { mutableStateOf(task.completed) }
@@ -52,53 +51,55 @@ fun TaskItem(
         modifier = Modifier
             .fillMaxWidth()
             .height(40.dp)
-            //.border(2.dp, DarkBrown)
             .clickable { showPopup = true }
     ) {
         Row(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
 
-            Box(
-                modifier = Modifier
-                    .width(4.dp)
-                    .fillMaxHeight(0.6f)
-                    .clip(RoundedCornerShape(500.dp))
-                    .background(Green)
-            )
+            Row() {
+                Box(
+                    modifier = Modifier
+                        .width(4.dp)
+                        .fillMaxHeight(0.6f)
+                        .clip(RoundedCornerShape(500.dp))
+                        .background(color)
+                ) // Barra vertical
 
-            Text(text = task.title, fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.width(8.dp))
+                Spacer(modifier = Modifier.width(6.dp))
+                Text(text = task.title, fontWeight = FontWeight.Bold, textDecoration = if (task.completed) TextDecoration.LineThrough else TextDecoration.None)
+            }
 
-            /*Text(
-                text = if (task.completed) "Completada" else "Pendiente",
-                color = if (task.completed) Color.Green else Color.Red
-            )*/
-
-            Checkbox(
+            // Aseguramos de que el checkbox esté alineado a la derecha, agregando un Spacer
+            Spacer(modifier = Modifier.weight(0.1f)) // Esto empuja el checkbox hacia la derecha
+            CustomCheckBox(
                 checked = checked,
                 onCheckedChange = { isChecked ->
                     checked = isChecked
                     taskViewModel.updateTaskCompletion(userId, task.id, isChecked)
-                }
+                },
             )
         }
     }
 
+
     // Popup de opciones
     if (showPopup) {
         AlertDialog(
+            modifier = Modifier,
+            containerColor = White,
             onDismissRequest = { showPopup = false },
             title = { Text(text = "Opciones") },
             text = {
                 Column {
                     TextButton(onClick = { onEdit(); showPopup = false }) {
-                        Text("Editar")
+                        Text("Editar", color = DarkBrown)
                     }
                     TextButton(onClick = { onDelete(); showPopup = false }) {
-                        Text("Eliminar")
+                        Text("Eliminar", color = DarkBrown)
                     }
                 }
             },
@@ -118,5 +119,6 @@ fun TaskItem(
         Spacer(modifier = Modifier.height(5.dp))
     }
 }
+
 
 
--- app/src/main/java/com/example/tfgonitime/ui/screens/home/homeScreen.kt
+++ app/src/main/java/com/example/tfgonitime/ui/screens/home/homeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.tfgonitime.ui.screens.home
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -7,7 +8,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -40,6 +44,15 @@ fun HomeScreen(
     val tasks by taskViewModel.tasksState.collectAsState()
     val groups by groupViewModel.groupsState.collectAsState()
 
+    val colorMap = mapOf(
+        "Green" to Green,
+        "DarkBrown" to DarkBrown,
+        "White" to White,
+        "Brown" to Brown,
+        "Gray" to Gray
+    )
+
+
     // Si el usuario no está autenticado, mostramos un mensaje
     if (userId == null) {
         Column(modifier = Modifier.padding(16.dp)) {
@@ -56,109 +69,128 @@ fun HomeScreen(
             containerColor = Color.White,
             bottomBar = { CustomBottomNavBar(navHostController) },
             content = { paddingValues ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(paddingValues)
-                ) {
-                    // Parte superior (40% de la pantalla)
-                    Box(
+                Box( modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)){
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight(0.4f)
-                            .background(White),
-                        contentAlignment = Alignment.TopCenter
+                            .fillMaxSize()
                     ) {
-                        Button(
-                            onClick = {
-                                navHostController.navigate("addTaskScreen")
-                            },
+                        // Parte superior (40% de la pantalla)
+                        Box(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(16.dp)
+                                .fillMaxHeight(0.35f)
+                                .background(White),
+                            contentAlignment = Alignment.TopCenter
                         ) {
-                            Text(text = "Añadir Tarea")
+
                         }
-                    }
 
-                    // Parte inferior (60% de la pantalla) con scroll
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-                            .background(Green)
-                    ) {
-                        LazyColumn(
+                        // Parte inferior (60% de la pantalla) con scroll
+                        Box(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .padding(start = 20.dp, end = 20.dp, top = 20.dp)
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+                                .background(Green.copy(alpha = 0.7f))
                         ) {
-                            item {
-                                Spacer(modifier = Modifier.height(40.dp))
-                            }
-
-                            items(groups) { group ->
-
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .clip(RoundedCornerShape(16.dp))
-                                        .background(White)
-                                        .padding(20.dp)
-                                ) {
-                                    Text(
-                                        group.groupName,
-                                        modifier = Modifier.fillMaxWidth(),
-                                        style = TextStyle(
-                                            fontWeight = FontWeight.SemiBold,
-                                            fontSize = 20.sp,
-                                            color = Brown
-                                        )
-                                    )
-
-                                    Spacer(modifier = Modifier.height(20.dp))
-
-                                    // Filtrar las tareas que corresponden a este grupo
-                                    val tasksForGroup = tasks.filter { task ->
-                                        task.groupId == group.groupId
-                                    }
-
-                                    // Si hay tareas, las mostramos
-                                    if (tasksForGroup.isNotEmpty()) {
-                                        tasksForGroup.forEachIndexed { index, task ->
-                                            TaskItem(
-                                                task = task,
-                                                navHostController = navHostController,
-                                                userId = userId,
-                                                onDelete = {
-                                                    taskViewModel.deleteTask(userId, task.id)
-                                                },
-                                                onEdit = {
-                                                    navHostController.navigate("editTaskScreen/${task.id}")
-                                                },
-                                                taskViewModel = taskViewModel,
-                                                index = index,
-                                                totalItems = tasksForGroup.size
-                                            )
-                                        }
-                                    } else {
-                                        // Si no hay tareas para este grupo, mostramos un mensaje
-                                        Text(
-                                            text = "No hay tareas para este grupo.",
-                                            style = TextStyle(fontSize = 16.sp, color = Color.Gray)
-                                        )
-                                    }
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(start = 20.dp, end = 20.dp, top = 20.dp)
+                            ) {
+                                item {
+                                    Spacer(modifier = Modifier.height(40.dp))
                                 }
 
-                                Spacer(modifier = Modifier.height(30.dp))
+                                items(groups) { group ->
+
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .clip(RoundedCornerShape(16.dp))
+                                            .background(White)
+                                            .padding(20.dp)
+                                    ) {
+                                        Text(
+                                            text = group.groupName,
+                                            modifier = Modifier.fillMaxWidth(),
+                                            style = TextStyle(
+                                                fontWeight = FontWeight.SemiBold,
+                                                fontSize = 20.sp,
+                                                color = colorMap[group.groupColor] ?: DarkBrown //Usar color del grupo
+                                            )
+                                        )
+
+                                        Spacer(modifier = Modifier.height(20.dp))
+
+                                        // Filtrar las tareas que corresponden a este grupo
+                                        val tasksForGroup = tasks.filter { task ->
+                                            task.groupId == group.groupId
+                                        }
+
+                                        // Si hay tareas, las mostramos
+                                        if (tasksForGroup.isNotEmpty()) {
+                                            tasksForGroup.forEachIndexed { index, task ->
+                                                TaskItem(
+                                                    task = task,
+                                                    userId = userId,
+                                                    onDelete = {
+                                                        taskViewModel.deleteTask(userId, task.id)
+                                                    },
+                                                    onEdit = {
+                                                        navHostController.navigate("editTaskScreen/${task.id}")
+                                                    },
+                                                    taskViewModel = taskViewModel,
+                                                    index = index,
+                                                    totalItems = tasksForGroup.size,
+                                                    color = colorMap[group.groupColor] ?: DarkBrown
+                                                )
+                                            }
+                                        } else {
+                                            // Si no hay tareas para este grupo, mostramos un mensaje
+                                            Text(
+                                                text = "No hay tareas para este grupo.",
+                                                style = TextStyle(fontSize = 16.sp, color = Color.Gray)
+                                            )
+                                        }
+                                    }
+
+                                    Spacer(modifier = Modifier.height(30.dp))
+
+                                }
                             }
                         }
+
                     }
+                    CustomFloatingButton { navHostController.navigate("addTaskScreen") }
                 }
+
             }
+
         )
     }
 }
 
 
+@Composable
+fun CustomFloatingButton(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        FloatingActionButton(
+            onClick = onClick,
+
+            contentColor = Color.White,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            shape = CircleShape,
+            containerColor = Brown
+
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Agregar")
+        }
+    }
+}
