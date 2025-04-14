6113fe62eeb08184aafb88de3560d98fdda86a33
Yilin <yilin.zh135@gmail.com>
2025-04-14 17:37:19 +0200

    update task item popup and reusable delete confirmation dialog

--- app/src/main/java/com/example/tfgonitime/ui/components/taskComp/TaskItem.kt
+++ app/src/main/java/com/example/tfgonitime/ui/components/taskComp/TaskItem.kt
@@ -1,8 +1,10 @@
 package com.example.tfgonitime.ui.components.taskComp
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,16 +15,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +48,11 @@ import com.example.tfgonitime.data.model.Task
 import com.example.tfgonitime.ui.components.CustomCheckBox
 import com.example.tfgonitime.ui.theme.*
 import com.example.tfgonitime.viewmodel.TaskViewModel
+import androidx.compose.material3.*
+import com.example.tfgonitime.ui.components.DeleteConfirmationDialog
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskItem(
     task: Task,
@@ -48,7 +65,11 @@ fun TaskItem(
     color: Color
 ) {
     var showPopup by remember { mutableStateOf(false) }
+    var showDeleteConfirmation by remember { mutableStateOf(false) } // Nuevo estado para la confirmación de eliminar
     var checked by remember { mutableStateOf(task.completed) }
+
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
 
     Box(
         modifier = Modifier
@@ -57,15 +78,13 @@ fun TaskItem(
             .clickable { showPopup = true },
     ) {
         Row(
-            modifier = Modifier
-                .fillMaxSize(),
+            modifier = Modifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Row(
                 modifier = Modifier.fillMaxHeight(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Box(
                     modifier = Modifier
@@ -73,7 +92,7 @@ fun TaskItem(
                         .fillMaxHeight(0.6f)
                         .clip(RoundedCornerShape(500.dp))
                         .background(color)
-                ) // Barra vertical
+                )
 
                 Spacer(modifier = Modifier.width(6.dp))
 
@@ -89,35 +108,83 @@ fun TaskItem(
                 onCheckedChange = { isChecked ->
                     checked = isChecked
                     taskViewModel.updateTaskCompletion(userId, task.id, isChecked)
-                },
+                }
             )
         }
     }
 
-
-    // Popup de opciones
     if (showPopup) {
-        AlertDialog(
-            modifier = Modifier,
-            containerColor = White,
+        ModalBottomSheet(
             onDismissRequest = { showPopup = false },
-            title = { Text(text = "Opciones") },
-            text = {
-                Column {
-                    TextButton(onClick = { onEdit(); showPopup = false }) {
-                        Text("Editar", color = DarkBrown)
-                    }
-                    TextButton(onClick = { onDelete(); showPopup = false }) {
-                        Text("Eliminar", color = DarkBrown)
-                    }
+            sheetState = sheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            scope.launch { sheetState.hide() }.invokeOnCompletion { showPopup = false }
+                            onEdit()
+                        }
+                        .padding(vertical = 8.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Editar",
+                        tint = DarkBrown,
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "Editar Tarea",
+                        color = DarkBrown,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
                 }
-            },
-            confirmButton = {},
-            dismissButton = {}
-        )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            scope.launch { sheetState.hide() }.invokeOnCompletion { showPopup = false }
+                            showDeleteConfirmation = true
+                        }
+                        .padding(vertical = 8.dp)
+                ) {
+                    Icon(
+
+                        imageVector = Icons.Default.Delete, // Aquí cambiamos al ícono de papelera
+                        contentDescription = "Eliminar",
+                        tint = Green,
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "Eliminar Tarea",
+                        color = Green,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
     }
 
-    // Divider entre tareas, excepto en la última
+
+    DeleteConfirmationDialog(
+        showDialog = showDeleteConfirmation,
+        onDismiss = { showDeleteConfirmation = false },
+        onConfirm = onDelete
+    )
+
+
     if (index != totalItems - 1) {
         Spacer(modifier = Modifier.height(5.dp))
         HorizontalDivider(
@@ -127,8 +194,4 @@ fun TaskItem(
         )
         Spacer(modifier = Modifier.height(5.dp))
     }
-
 }
-
-
-
