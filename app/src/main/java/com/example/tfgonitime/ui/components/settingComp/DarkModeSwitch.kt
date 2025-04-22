01c6029c16e26882fc6ef9f9a509f2bc2f678b21
kiwan33 <rumansillar@gmail.com>
2025-04-22 17:08:58 +0200

    Add dark mode logic

---
+++ app/src/main/java/com/example/tfgonitime/ui/components/settingComp/DarkModeSwitch.kt
@@ -0,0 +1,27 @@
+package com.example.tfgonitime.ui.components.settingComp
+
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.tfgonitime.ui.theme.Green
+import com.example.tfgonitime.ui.theme.White
+
+@Composable
+fun DarkModeSwitch(
+    isDarkTheme: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Switch(
+        checked = isDarkTheme,
+        onCheckedChange = onCheckedChange,
+        modifier = modifier,
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = White,
+            uncheckedThumbColor = Green,
+            checkedTrackColor = Green,
+            uncheckedTrackColor = White
+        )
+    )
+}
