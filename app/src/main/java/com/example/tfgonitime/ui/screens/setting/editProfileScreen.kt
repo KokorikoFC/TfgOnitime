542ff993347c0bd12d584f12cef74eac96fbb64d
Hugo Sanz <hugoestudiando1@gmail.com>
2025-05-11 23:47:16 +0200

    Updated edit profile screen style

--- app/src/main/java/com/example/tfgonitime/ui/screens/setting/editProfileScreen.kt
+++ app/src/main/java/com/example/tfgonitime/ui/screens/setting/editProfileScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,8 +18,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -44,6 +49,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.wear.compose.material3.IconButton
 import com.example.tfgonitime.R
 import com.example.tfgonitime.ui.components.CustomBottomNavBar
 import com.example.tfgonitime.viewmodel.AuthViewModel
@@ -88,192 +94,150 @@ fun EditProfileScreen(
 
     var firstName by remember { mutableStateOf("") }
     var lastName by remember { mutableStateOf("") }
+
     Scaffold(
         containerColor = Color.White,
         bottomBar = { CustomBottomNavBar(navHostController) },
         content = { paddingValues ->
 
-            LazyColumn(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(paddingValues),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+                    .padding(start = 16.dp, end = 16.dp, top = 50.dp) // Márgenes
             ) {
-                item {
-                    val settingsText = stringResource(R.string.settings_edit_profile)
+                // Cabecera con el botón de volver atrás
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .padding(bottom = 24.dp)
+                        .fillMaxWidth()
+                ) {
+                    IconButton(
+                        onClick = { navHostController.popBackStack() },
+                        modifier = Modifier.size(24.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Volver atrás",
+                            tint = Color.Black
+                        )
+                    }
+
                     Text(
-                        text = settingsText,
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black
-                        ),
-                        modifier = Modifier
-                            .padding(bottom = 24.dp)
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.Center
+                        text = stringResource(R.string.settings_edit_profile),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = Color.Black
                     )
+
+                    Spacer(modifier = Modifier.size(24.dp)) // Para balancear visualmente
                 }
 
-                item {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        contentAlignment = Alignment.Center,
-
-                    ) {
+                // Contenido de la pantalla (como las imágenes, los campos de texto, etc.)
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    item {
                         Box(
                             modifier = Modifier
-                                .size(115.dp) // Tamaño del círculo
-                        ){
-                            // Círculo gris grande con imagen de perfil (debe ir PRIMERO)
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            contentAlignment = Alignment.Center,
+                        ) {
                             Box(
                                 modifier = Modifier
-                                    .fillMaxSize()
-                                    .clip(CircleShape)
+                                    .size(115.dp) // Tamaño del círculo
                             ) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.emotionface_happy),
-                                    contentDescription = "Imagen de perfil",
+                                // Círculo gris grande con imagen de perfil (debe ir PRIMERO)
+                                Box(
                                     modifier = Modifier
                                         .fillMaxSize()
                                         .clip(CircleShape)
-                                )
-                            }
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.emotionface_happy),
+                                        contentDescription = "Imagen de perfil",
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .clip(CircleShape)
+                                    )
+                                }
 
-                            // Círculo negro con el ícono de "plus" (debe ir DESPUÉS)
-                            Box(
-                                modifier = Modifier
-                                    .size(30.dp) // Tamaño del botón flotante
-                                    .clip(CircleShape)
-                                    .background(Color.Black) // Fondo negro para hacer visible el botón
-                                    .align(Alignment.BottomEnd), // Lo coloca sobre la imagen
-                                //.offset(x = 5.dp, y = 5.dp), // Ajusta la posición
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.emotionface_plus),
-                                    contentDescription = "Imagen para editar foto de perfil",
+                                // Círculo negro con el ícono de "plus" (debe ir DESPUÉS)
+                                Box(
                                     modifier = Modifier
-                                        .size(30.dp) // Tamaño de la imagen dentro del botón
+                                        .size(30.dp) // Tamaño del botón flotante
                                         .clip(CircleShape)
-                                )
+                                        .background(Color.Black) // Fondo negro para hacer visible el botón
+                                        .align(Alignment.BottomEnd), // Lo coloca sobre la imagen
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.emotionface_plus),
+                                        contentDescription = "Imagen para editar foto de perfil",
+                                        modifier = Modifier
+                                            .size(30.dp) // Tamaño de la imagen dentro del botón
+                                            .clip(CircleShape)
+                                    )
+                                }
                             }
                         }
-
                     }
-                }
 
-
-
-                item {
-                    val nameText = stringResource(R.string.name_hint)
-                    Text(
-                        text = nameText,
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black
-                        ),
-                        modifier = Modifier.padding(top = 8.dp, bottom = 4.dp)
-                    )
-                }
-
-                item {
-                    HorizontalDivider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 3.dp),
-                        thickness = 2.dp,
-                        color = Color.Black
-                    )
-                }
-
-                item {
-
-                    TextField(
-                        value = firstName,
-                        onValueChange = { firstName = it },
-                        placeholder = { Text( stringResource(R.string.name_hint)) },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(55.dp)
-                            .clip(MaterialTheme.shapes.medium)
-                            .border(
-                                1.dp,
-                                Color.Gray,
-                                shape = MaterialTheme.shapes.medium
-                            ), // Borde personalizado
-                        colors = TextFieldDefaults.colors(
-                            focusedContainerColor = Color.Transparent, // Sin fondo al enfocar
-                            unfocusedContainerColor = Color.Transparent, // Sin fondo al desenfocar
-                            focusedIndicatorColor = Color.Transparent, // Sin línea de indicador al enfocar
-                            unfocusedIndicatorColor = Color.Transparent, // Sin línea de indicador al desenfocar
-                            cursorColor = Color.Black, // Cursor negro
+                    item {
+                        val nameText = stringResource(R.string.name_hint)
+                        Text(
+                            text = nameText,
+                            style = TextStyle(
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            ),
+                            modifier = Modifier.padding(top = 8.dp, bottom = 4.dp)
                         )
-                    )
-                }
-                item {
-                    val genderText = stringResource(R.string.profile_gender_text)
-                    Text(
-                        text = genderText,
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black
-                        ),
-                        modifier = Modifier.padding(top = 8.dp, bottom = 1.dp)
-                    )
-                }
-
-                item {
-                    HorizontalDivider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 1.dp, bottom = 3.dp),
-                        thickness = 1.dp,
-                        color = Color.Black
-                    )
-                }
-
-                item {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp), // Espacio entre botones
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Button(
-                            onClick = { /* Acción para Masculino */ },
-                            modifier = Modifier.weight(1f) // Asegura que los botones se distribuyan equitativamente
-                        ){
-                            val genderMale = stringResource(R.string.gender_male)
-                            Text(text = genderMale)
-                        }
-
-                        Button(
-                            onClick = { /* Acción para Femenino */ },
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            val genderFemale = stringResource(R.string.gender_female)
-                            Text(text = genderFemale)
-                        }
-
-                        Button(
-                            onClick = { /* Acción para Otro */ },
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            val genderOther = stringResource(R.string.gender_other)
-                            Text(text = genderOther)
-                        }
                     }
+
+                    item {
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.Black
+                        )
+                    }
+
+                    item {
+
+                        TextField(
+                            value = firstName,
+                            onValueChange = { firstName = it },
+                            placeholder = { Text(stringResource(R.string.name_hint)) },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(55.dp)
+                                .clip(MaterialTheme.shapes.medium)
+                                .border(
+                                    1.dp,
+                                    Color.Gray,
+                                    shape = MaterialTheme.shapes.medium
+                                ),
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = Color.Transparent,
+                                unfocusedContainerColor = Color.Transparent,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                cursorColor = Color.Black
+                            )
+                        )
+                    }
+                    // Resto del contenido...
                 }
-
-
-
-            }//Aquí se cierra el LazyColum
-        } // Aquí se cierra el bloque content
-    ) // Aquí se cierra el Scaffold
-} // Aquí se cierra la función EditProfileSettingScreen
-
-
+            }
+        }
+    )
+}
